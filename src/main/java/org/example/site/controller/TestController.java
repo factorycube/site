c1eae5dd48995a3d9eb3864e8a70b7405a4a5955
factorycube <gwan1020@gmail.com>
2020-09-09 02:45:25 +0900

    [ADD] 초기 셋팅 커밋.

---
+++ src/main/java/org/example/site/controller/TestController.java
@@ -0,0 +1,15 @@
+package org.example.site.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+    @RequestMapping("/test.do")
+    public String home(Model model) {
+        model.addAttribute("test", "hello world!");
+
+        return "test";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/test.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Test Page</title>
+</head>
+<body>
+    <h2>Test Page...</h2>
+
+    <p>${test}</p>
+</body>
+</html>
